0bc60e2952aef3648f9c174bc8f92d70feeaaa2a
Trong Tin <tin.nguyen.cs05@gmail.com>
2025-11-17 23:56:38 +0700

    update menu, add model and data

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
-from abc import abstractmethod, ABC
+from os import name
+
 from sqlalchemy import Column, Integer, DateTime, String, Float, Boolean, ForeignKey, Enum, Date, Text
 from app import db,app
 from datetime import datetime, date
@@ -112,10 +113,17 @@ class Waiter(Employee):
         'polymorphic_identity': EmployeeRole.WAITER
     }
 
+class CategoryGroup(BaseModel):
+    name = Column(String(50), unique=True, nullable=False)
+    categories = relationship('DishCategory', backref='categoryGroup', lazy=True)
+
+    def __str__(self):
+        return self.name
 
 class DishCategory(BaseModel):
     name = Column(String(50), unique=True, nullable=False)
     dishes = relationship('Dish', backref='dishCategory', lazy=True)
+    categoryGroup_id = Column(Integer, ForeignKey(CategoryGroup.id), nullable=False)
 
     def __str__(self):
         return self.name
@@ -242,6 +250,8 @@ class InventoryReportDetail(BaseModel):
     inventoryReport_id = Column(Integer, ForeignKey(InventoryReport.id), nullable=False)
     ingredient_id = Column(Integer, ForeignKey(Ingredient.id), nullable=False)
 
+
+
 # class PaymentStrategy(db.Model, ABC):
 #     __abstract__ = True
 #
@@ -266,6 +276,33 @@ if __name__ == "__main__":
     with app.app_context():
         db.drop_all()
         db.create_all()
-        c1 = DishCategory(name ='Latte')
-        db.session.add(c1)
+        cg1 = CategoryGroup(name='Foods')
+        cg2 = CategoryGroup(name='Drinks')
+        dc1 = DishCategory(name='Latte', categoryGroup_id=2)
+        dc2 = DishCategory(name='Hot Coffee', categoryGroup_id=2)
+        dc3 = DishCategory(name='Breakfast', categoryGroup_id=1)
+        dc4 = DishCategory(name='Hot Chocolate', categoryGroup_id=2)
+        dc5 = DishCategory(name='Bakery', categoryGroup_id=1)
+        d1 = Dish(name='Latte', price=40000,
+                  image='https://res.cloudinary.com/dam6k8ezg/image/upload/v1762667757/kices3owtzqzhhxqshie.jpg',
+                  unit=DishUnit.CUP, dishCategory_id=1)
+        d2 = Dish(name='Americano', price=40000,
+                  image='https://res.cloudinary.com/dam6k8ezg/image/upload/v1762668228/nk9biuw7piporcr5ykoa.jpg',
+                  unit=DishUnit.CUP, dishCategory_id=2)
+        d3 = Dish(name='Matcha Latte', price=40000,
+                  image='https://res.cloudinary.com/dam6k8ezg/image/upload/v1762668374/nlai0rzggdsex7bu0mxe.jpg',
+                  unit=DishUnit.CUP, dishCategory_id=1)
+        d4 = Dish(name='Croissant', price=40000,
+                  image='https://res.cloudinary.com/dam6k8ezg/image/upload/v1762668533/qbnmz5iknk5dvuxfb8od.jpg',
+                  unit=DishUnit.PIECE, dishCategory_id=5)
+        d5 = Dish(name='Bacon Gouda Egg Sandwich', price=40000,
+                  image='https://res.cloudinary.com/dam6k8ezg/image/upload/v1762668577/uu2adt55oktzh93j9zlb.jpg',
+                  unit=DishUnit.PIECE, dishCategory_id=3)
+        d6 = Dish(name='Cappuccino', price=40000,
+                  image='https://res.cloudinary.com/dam6k8ezg/image/upload/v1762668604/xvifqkcn1ukgexznfpby.jpg',
+                  unit=DishUnit.CUP, dishCategory_id=2)
+        d7 = Dish(name='Egg Pesto Mozzarella Sandwich', price=40000,
+                  image='https://res.cloudinary.com/dam6k8ezg/image/upload/v1762668656/yxfejhpbb4g3svpgtsvq.jpg',
+                  unit=DishUnit.PIECE, dishCategory_id=3)
+        db.session.add_all([cg1, cg2, dc1, dc2, dc3, dc4, dc5, d1, d2, d3, d4, d5, d6, d7])
         db.session.commit()
